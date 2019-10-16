db817722ffc2e8ecb88deb970e35f6cff4284afd
Inessa Vasilevskaya <ivasilev@redhat.com>
2019-10-16 16:34:44 +0200

    Fixed preupgrade failure if vm names didn't match
    Previously if vm's hostname was different from the
    foreman name the preupgrade template job would fail
    with cryptic error. That was fixed by:
    - passing actual vm's foreman name as one of the params
      of preupgrade_report upload request;
    - adding a check in create method that would ensure a
      host with given name is known to foreman.

--- app/controllers/api/v2/preupgrade_reports_controller.rb
+++ app/controllers/api/v2/preupgrade_reports_controller.rb
@@ -3,13 +3,21 @@
 module Api
   module V2
     class PreupgradeReportsController < ::Api::V2::BaseController
+      before_action :resolve_host, only: [:create]
+      def resolve_host
+        host_name_or_id = params['host']
+        @host = Host.where(:name => host_name_or_id).or(Host.where(:id => host_name_or_id)).first
+      end
+
       def create
         date = DateTime.now.utc
-        host_name_or_id = params['host']
         status = params['status']
-        host = Host.where(:name => host_name_or_id).or(Host.where(:id => host_name_or_id)).first
-        report = PreupgradeReport.create_report(host, date, status, params['preupgrade_report'])
-        render :json => report
+        if @host.nil?
+          render :json => { "error": format("Couldn't find host %<host>s", host: params['host']) }
+        else
+          report = PreupgradeReport.create_report(@host, date, status, params['preupgrade_report'])
+          render :json => report
+        end
       end
 
       def show
