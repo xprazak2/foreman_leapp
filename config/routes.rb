5dee842a1c266ff0581595b80ff2098594a9ef24
Inessa Vasilevskaya <ivasilev@redhat.com>
2019-10-21 16:26:18 +0000

    Added show last report per host
    GET /api/v2/preupgrade_reports/hosts/HOST_NAME_OR_ID/last
    will return last stored report for given host.
    
    TBD Think about proper ruby-controller style default routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     scope '(:apiv)', :module => :v2, :defaults => { :apiv => 'v2' }, :apiv => /v2/,
                      :constraints => ApiConstraints.new(:version => 2, :default => true) do
       resources :preupgrade_reports, only: %i[show create]
+      # XXX FIXME find a way to utilize built-in routing
+      get '/preupgrade_reports/hosts/:host/last', :to => 'preupgrade_reports#last_report'
     end
   end
 end
