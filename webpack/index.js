dc11c473e1d2fb71831f87508f1cd9268f2875fd
Fellipe Henrique <fpedrosa@redhat.com>
2020-03-12 16:49:24 -0300

    .

---
+++ webpack/index.js
@@ -0,0 +1,15 @@
+/* eslint import/no-unresolved: [2, { ignore: [foremanReact/*] }] */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+
+import componentRegistry from 'foremanReact/components/componentRegistry';
+import Application from './containers/Application/index';
+import './redux';
+// Not currently mocking anything
+// import './services/api/setupMocks';
+
+/*componentRegistry.register({
+  name: 'leapp',
+  type: Application,
+});*/
