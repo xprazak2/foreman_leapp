cfc8635c47f092f0a10dc4fb9e27c47b90664892
Ondrej Prazak <oprazak@redhat.com>
2020-03-25 12:58:25 +0100

    wip

--- webpack/components/JobInvocationLeappTab/JobInvocationLeappTab.js
+++ webpack/components/JobInvocationLeappTab/JobInvocationLeappTab.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { LeappTest } from 'leapp-report-ui/dist/leapp-report-ui';
-import * as leapp from 'leapp-report-ui/leapp';
+
+console.log(LeappTest);
 
 const JobInvocationLeappTab = props => {
   return (
     <LeappTest />
   )
-
-  // return (
-  //   <div>LT</div>
-  // )
 }
 
 export default JobInvocationLeappTab;
