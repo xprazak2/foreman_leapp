a8c5688c7b02d8fd73dc0ddb520123f7ad483527
Ondrej Prazak <oprazak@redhat.com>
2020-03-31 08:50:30 +0200

    Add 'Fix All' button for preupgrade entries

--- webpack/components/PreupgradeReports/PreupgradeReportsSelectors.js
+++ webpack/components/PreupgradeReports/PreupgradeReportsSelectors.js
@@ -2,6 +2,7 @@ export const selectPreupgrade = state => state.foremanLeapp.preupgrade;
 
 export const selectPreupgradeReports = state =>
   selectPreupgrade(state).preupgradeReports;
+
 export const selectLoadingPreupgradeReports = state =>
   selectPreupgrade(state).loadingPreupgradeReports;
 export const selectError = state => selectPreupgrade(state).error;
--- webpack/components/PreupgradeReportsList/index.js
+++ webpack/components/PreupgradeReportsList/index.js
@@ -4,23 +4,16 @@ import PropTypes from 'prop-types';
 
 import PreupgradeReportEntry from './components/PreupgradeReportEntry';
 
-const PreupgradeReportsList = ({ preupgradeReports }) => {
-  const flatEntries = preupgradeReports.reduce(
-    (memo, report) => [...memo, ...report.entries],
-    []
-  );
-
-  return (
-    <ListView>
-      {flatEntries.map((entry, idx) => (
-        <PreupgradeReportEntry entry={entry} key={idx} />
-      ))}
-    </ListView>
-  );
-};
+const PreupgradeReportsList = ({ allEntries }) => (
+  <ListView>
+    {allEntries.map((entry, idx) => (
+      <PreupgradeReportEntry entry={entry} key={idx} />
+    ))}
+  </ListView>
+);
 
 PreupgradeReportsList.propTypes = {
-  preupgradeReports: PropTypes.array.isRequired,
+  allEntries: PropTypes.array.isRequired,
 };
 
 export default PreupgradeReportsList;
