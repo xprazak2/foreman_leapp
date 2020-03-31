06ba0c1e69688160673f22c549d8378510e71327
Ondrej Prazak <oprazak@redhat.com>
2020-03-31 14:03:53 +0200

    clean up

--- webpack/components/PreupgradeReports/PreupgradeReportsSelectors.js
+++ webpack/components/PreupgradeReports/PreupgradeReportsSelectors.js
@@ -3,31 +3,8 @@ import Immutable from 'seamless-immutable';
 export const selectPreupgrade = state => state.foremanLeapp.preupgrade;
 
 export const selectPreupgradeReports = state =>
-  selectPreupgrade(state).preupgradeReports//.map(transfromEntryAttrs);
+  selectPreupgrade(state).preupgradeReports;
 
 export const selectLoadingPreupgradeReports = state =>
   selectPreupgrade(state).loadingPreupgradeReports;
 export const selectError = state => selectPreupgrade(state).error;
-
-
-const transfromEntryAttrs = report => {
-
-  // const transformedEntries = report.entries.map(entry => {
-  //   console.log(entry);
-  //   console.log(entry.detail);
-
-  //   try {
-  //     // const tags = JSON.parse(entry.tags);
-  //     const detail = JSON.parse(entry.datail);
-  //   } catch(e) {
-  //     console.log(e)
-  //     console.error('Failed to parse report entry:', entry);
-  //     return entry;
-  //   }
-
-  //   return ({ ...entry, tags, detail });
-  // });
-
-  // return ({ ...report, entries: transformedEntries });
-  return report;
-}
