7468edbddf8b29fb744ade7a635fa110bd8ebdcc
Leos Stejskal <lstejska@redhat.com>
2020-04-15 14:22:22 +0200

    Leapp - Fix issue for SQlite migration

--- db/migrate/20200327091009_add_host_to_report.rb
+++ db/migrate/20200327091009_add_host_to_report.rb
@@ -1,6 +1,12 @@
 class AddHostToReport < ActiveRecord::Migration[5.2]
-  def change
-    add_column :preupgrade_report_entries, :host_id, :integer, null: false
+  def up
+    add_column :preupgrade_report_entries, :host_id, :integer
+    change_column :preupgrade_report_entries, :host_id, :integer, null: false
     add_index :preupgrade_report_entries, :host_id
+  end
+
+  def down
+    remove_index :preupgrade_report_entries, :host_id
+    remove_column :preupgrade_report_entries, :host_id
   end
 end
