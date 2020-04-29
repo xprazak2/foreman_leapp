3768a76896b68e0a6ad60bb8b2bc235005732372
Leos Stejskal <github@stejskalleos.cz>
2020-04-29 12:13:26 +0200

    Update 20200429080939_report_entries_flags.rb

--- db/migrate/20200429080939_report_entries_flags.rb
+++ db/migrate/20200429080939_report_entries_flags.rb
@@ -1,5 +1,5 @@
 class ReportEntriesFlags < ActiveRecord::Migration[6.0]
   def change
-    add_column :preupgrade_report_entries, :flags, :string
+    add_column :preupgrade_report_entries, :flags, :text
   end
 end
