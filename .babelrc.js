cf821feaf7e5a0be5e620e76a52ac7f95a5710c0
Ondrej Prazak <oprazak@redhat.com>
2020-05-07 15:17:30 +0200

    Transform svg in tests

--- .babelrc.js
+++ .babelrc.js
@@ -1,22 +1,3 @@
 module.exports = {
   presets: ['@theforeman/builder/babel'],
-  transformIgnorePatterns: ["^.+\\.svg$"],
-  transform: {
-    "^.+\\.svg$": "jest-svg-transformer"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[hash]-[name].[ext]',
-            },
-          },
-        ],
-      },
-    ],
-  },
 };
